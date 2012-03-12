82f182dfd258a4e932257eaa97260a80c1674292
nicho <nicho.liang@china-rewards.com>
2012-03-12 17:23:17 +0800

    dao

---
+++ core/src/main/java/com/chinarewards/elt/dao/org/RestaurantDao.java
@@ -0,0 +1,10 @@
+package com.chinarewards.elt.dao.org;
+
+import com.chinarewards.elt.common.BaseDao;
+import com.chinarewards.elt.domain.org.Restaurant;
+
+public class RestaurantDao extends BaseDao<Restaurant> {
+
+	
+	
+}
---
+++ core/src/main/java/com/chinarewards/elt/dao/person/PersonDao.java
@@ -0,0 +1,10 @@
+package com.chinarewards.elt.dao.person;
+
+import com.chinarewards.elt.common.BaseDao;
+import com.chinarewards.elt.domain.person.Person;
+
+public class PersonDao extends BaseDao<Person> {
+
+	
+	
+}
